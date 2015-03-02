548a7418e7368e9d2aa483b68cc7159ebfdfeb61
Kris Kemper <kriskemper@gmail.com>
2015-03-02 09:01:36 -0500

    Escape more method names

--- lib/cequel/metal/updater.rb
+++ lib/cequel/metal/updater.rb
@@ -59,7 +59,7 @@ module Cequel
       # @see DataSet#list_append
       #
       def list_append(column, elements)
-        statements << "#{column} = #{column} + [?]"
+        statements << "\"#{column}\" = \"#{column}\" + [?]"
         bind_vars << elements
       end
 
@@ -73,7 +73,7 @@ module Cequel
       # @see DataSet#list_remove
       #
       def list_remove(column, value)
-        statements << "#{column} = #{column} - [?]"
+        statements << "\"#{column}\" = \"#{column}\" - [?]"
         bind_vars << value
       end
 
@@ -88,7 +88,7 @@ module Cequel
       # @see DataSet#list_replace
       #
       def list_replace(column, index, value)
-        statements << "#{column}[#{index}] = ?"
+        statements << "\"#{column}\"[#{index}] = ?"
         bind_vars << value
       end
 
@@ -102,7 +102,7 @@ module Cequel
       # @see DataSet#set_add
       #
       def set_add(column, values)
-        statements << "#{column} = #{column} + {?}"
+        statements << "\"#{column}\" = \"#{column}\" + {?}"
         bind_vars << values
       end
 
@@ -116,7 +116,7 @@ module Cequel
       # @see DataSet#set_remove
       #
       def set_remove(column, values)
-        statements << "#{column} = #{column} - {?}"
+        statements << "\"#{column}\" = \"#{column}\" - {?}"
         bind_vars << ::Kernel.Array(values)
       end
 
@@ -131,7 +131,7 @@ module Cequel
       #
       def map_update(column, updates)
         binding_pairs = ::Array.new(updates.length) { '?:?' }.join(',')
-        statements << "#{column} = #{column} + {#{binding_pairs}}"
+        statements << "\"#{column}\" = \"#{column}\" + {#{binding_pairs}}"
         bind_vars.concat(updates.flatten)
       end
 
